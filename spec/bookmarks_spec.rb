c8f7718361df0cef4763a78aee15922a66f52fd4
Jordan <jordanmanu@live.com.au>
2022-04-13 16:21:39 +0100

    Passed failing tests

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -7,7 +7,7 @@ describe Bookmark do
     it 'lists all bookmarks' do
       add_test_bookmarks
       bookmarks = Bookmark.all
-      expect(bookmarks).to include "http://www.google.com"
-    end
+      expect(bookmarks).to include({title: "Google",url: "http://www.google.com"})
+    end                           
   end
 end
--- spec/db_helpers.rb
+++ spec/db_helpers.rb
@@ -7,5 +7,5 @@ end
 
 def add_test_bookmarks
     connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com'), ('http://www.destroyallsoftware.com'), ('http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.google.com', 'Google'), ('http://www.destroyallsoftware.com', 'Destroy All Software'), ('http://www.makersacademy.com', 'Makers');")
 end
